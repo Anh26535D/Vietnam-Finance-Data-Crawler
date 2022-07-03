82a31623d2a3de8c3e47a4e2269472991b7bf462
dangthevang <dangthevang2000@gmail.com>
2022-07-03 07:47:54 +0700

    del print

--- Crawl/base/setup.py
+++ Crawl/base/setup.py
@@ -138,7 +138,6 @@ class Setup():
         rs = requests.get(link)
         soup = BeautifulSoup(rs.content, 'html.parser')
         list_ = soup.find_all("div",{"class":"container"})
-        print(list_)
         if len(list_) == 0:
             return True
         else:
--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from Crawl import TVSI
 import multiprocessing
-path = '/content/drive/MyDrive/Data Lake/Ingestion/Day 0/TVSI/Financial/Quarter/BalanceSheet/'
-symbol = "AAA"
-com = TVSI.Financail(symbol=symbol)
-com.get_Balance(2000,2022).to_csv(symbol+".csv")
+import pandas as pd
+# path = '/content/drive/MyDrive/Data Lake/Ingestion/Day 0/TVSI/Financial/Quarter/BalanceSheet/'
+PATH = '/content/drive/MyDrive/Data Lake/Ingestion/Day 0/TVSI/Financial/Quarter/IncomeStatement/'
+
+def finan(symbol):
+    try:
+        df = pd.read_csv(PATH+symbol+".csv")
+    except:
+        print(symbol,end=" ")
+        com = TVSI.Financail(symbol=symbol)
+        com.get_Income(2000,2022).to_csv(PATH+symbol+".csv",index=False)
+        print(symbol,"Done!!!")
+
+data = pd.read_excel("/content/List_Com_Phase1.xlsx")
+Symbol = data["Symbol"]
+
+def multip():
+    pool = multiprocessing.Pool(processes=4)
+    for symbol in Symbol:
+        pool.apply_async(finan,args=(symbol,))
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+    multip()
+
