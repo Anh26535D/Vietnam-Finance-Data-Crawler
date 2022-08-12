37c5f493919e0200c1d8a510849a7a0963ab336b
dangthevang <dangthevang2000@gmail.com>
2022-08-12 23:47:47 +0000

    StockBiz

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
+
 from Crawl import TVSI
-from Crawl import CafeF
+from Crawl import StockBiz
 # import multiprocessing
 import pandas as pd
 # path = '/content/drive/MyDrive/Data Lake/Ingestion/Day 0/TVSI/Financial/Quarter/BalanceSheet/'
 # PATH = '/content/drive/MyDrive/Data Lake/Ingestion/Day 0/TVSI/Financial/Quarter/IncomeStatement/'
-PATH = ''
-# def close(symbol):
-#     try:
-#         df = pd.read_csv(PATH+symbol+".csv")
-#     except:
-#         com = CafeF.Close(symbol=symbol)
-#         com.DownloadCloseDetail().to_csv(f"{PATH}{symbol}.csv")
-def finan(symbol):
+PATH = '/content/drive/MyDrive/Data Lake/Ingestion/Day 0/StockBiz/Close/'
+def close(symbol):
     try:
         df = pd.read_csv(PATH+symbol+".csv")
     except:
-        print(symbol,end=" ")
-        com = TVSI.Financail(symbol=symbol)
-        com.get_Balance_Year(2000,2022).to_csv(PATH+symbol+".csv",index=False)
-        print(symbol,"Done!!!")
-finan("AAA")
+        com = StockBiz.Close(symbol=symbol)
+        com.DownloadClose().to_csv(f"{PATH}{symbol}.csv")
+# def finan(symbol):
+#     try:
+#         df = pd.read_csv(PATH+symbol+".csv")
+#     except:
+#         print(symbol,end=" ")
+#         com = TVSI.Financail(symbol=symbol)
+#         com.get_Balance_Year(2000,2022).to_csv(PATH+symbol+".csv",index=False)
+#         print(symbol,"Done!!!")
+# finan("AAA")
 
-# # data = pd.read_excel("/content/List_Com_Phase1.xlsx")
-# # Symbol = data["Symbol"]
-
+data = pd.read_csv("/content/List_Com_DateUpDownExchange.xlsx - Sheet1.csv")
+Symbol = data["Symbol"]
+for i in Symbol[1000:]:
+    try:
+        print(i,end="")
+        close(i)
+    except:
+        print("---loi")
+    print()
 # def multip():
 #     pool = multiprocessing.Pool(processes=4)
 #     for symbol in Symbol:
