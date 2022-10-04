59ff7eba9037abf4be8e497715d9f9d329894777
NgoXuanPhong <xuanphongcht@gmail.com>
2022-10-04 11:03:10 +0700

    no message

--- RunCrawl/crawl_volume.py
+++ RunCrawl/crawl_volume.py
@@ -16,18 +16,43 @@ def VolumeCafeF(symbol):
         df = pd.read_csv(f"{PATH}/VolumeNow/{symbol}.csv")
     except:
         com = CafeF.Volume()
-        com.getVolumeNow(symbol).to_csv(f"{PATH}/VolumeNow/{symbol}.csv",index=False)
-        com.getVolumeEvent(symbol).to_csv(f"{PATH}/VolumeAdditionailEvents/{symbol}.csv",index=False)
-    
+        try:
+            com.getVolumeNow(symbol).to_csv(f"{PATH}/VolumeNow/{symbol}.csv",index=False)
+        except:
+            pass
+        try:
+            com.getVolumeEvent(symbol).to_csv(f"{PATH}/VolumeAdditionailEvents/{symbol}.csv",index=False)
+        except:
+            pass
+
+def run_reset_vs():
+    global com
+    try:
+        com = VietStock.Other()
+    except:
+        print("Tam Nghi VS-------------------")
+        run_reset_vs()
+        
+            
 com = VietStock.Other()
 def VolumeVietStock(symbol):
     PATH = PATH_.joinPath(PATH_.PATH_VOLUME,"VietStock")
     try:
         df = pd.read_csv(f"{PATH}/{symbol}.csv")
     except:
-        com.AdditionalListing(symbol).to_csv(f"{PATH}/VolumeAdditionailEvents/{symbol}.csv",index=False)
-        com.VolumeNow(symbol).to_csv(f"{PATH}/VolumeNow/{symbol}.csv",index=False)
-        com.TreasuryStockTransactions(symbol).to_csv(f"{PATH}/TreasuryShares/{symbol}.csv",index=False)
+        print(f"{PATH}/VolumeAdditionailEvents/{symbol}.csv")
+        try:
+            com.AdditionalListing(symbol).to_csv(f"{PATH}/VolumeAdditionailEvents/{symbol}.csv",index=False)
+        except:
+            run_reset_vs()
+        try:
+            com.VolumeNow(symbol).to_csv(f"{PATH}/VolumeNow/{symbol}.csv",index=False)
+        except:
+            run_reset_vs()
+        try:
+            com.TreasuryStockTransactions(symbol).to_csv(f"{PATH}/TreasuryShares/{symbol}.csv",index=False)
+        except:
+            run_reset_vs()
 
 List_Symbol = pd.read_csv(f'{PATH_.joinPath(PATH_.PATH_MAIN_CURRENT,"List_company")}.csv')
 for symbol in List_Symbol["Mã CK▲"]:
